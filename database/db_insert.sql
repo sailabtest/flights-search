234fcdd699d74be2117e38b2cd4b9cd13cf9960d
Sailab Rahimi <74360876+sailabtest@users.noreply.github.com>
2025-01-12 23:55:23 +0000

    Add files via upload

---
+++ database/db_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO flight (airline, flight_number, departure_city, destination_city, departure_time, arrival_time, price, flight_distance) VALUES 
+('Emirate', 'E01', 'Dubai', 'London', '2025-01-01 01:00:00', '2025-01-012 11:00:00', 540.00, 3200),
+('British Airline', 'BA01', 'London', 'Paris', '2025-02-02 03:00:00', '2025-02-02 05:00:00', 420.00, 320),
+('American Airline', 'AA01', 'Paris', 'Berlin', '2025-01-03 10:00:00', '2025-01-03 21:00:00', 200.00, 290),
+('Blue Jet', 'BJ01', 'Rome', 'Moscow', '2025-01-04 15:00:00', '2025-02-07 15:00:00', 490.00, 1000),
+('Easyjet', 'EJ01', 'London', 'Leads', '2025-01-05 13:00:00', '2025-02-09 11:00:00', 25.00, 200);
